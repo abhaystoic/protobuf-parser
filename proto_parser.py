204ab8956a89bdce51ecfe440bdb6d7496b32597
abhaystoic <iabhaygupta90@gmail.com>
2017-06-21 22:48:19 +0530

    A simple protobuf parser library

---
+++ proto_parser.py
@@ -0,0 +1,28 @@
+import addressbook_pb2
+
+_desc = addressbook_pb2.DESCRIPTOR
+
+class ProtoParser:
+  """docstring for ClassName"""
+  def __init__(self):
+    self.columns = []
+
+  def decide_and_prepare(self, key, value):
+    for k in value.fields_by_name.values():
+      if k.type != 11:
+        self.columns.append(k.full_name)
+      else:
+        self.parse_proto(value, 'nested')
+
+  def parse_proto(self, desc, type='normal'):
+    if type == 'normal':
+      for key, value in desc.message_types_by_name.items():
+        self.decide_and_prepare(key, value)
+    else:
+      for key, value in desc.nested_types_by_name.items():
+        self.decide_and_prepare(key, value)
+
+if __name__ == "__main__":
+  parserObj = ProtoParser()
+  parserObj.parse_proto(_desc)
+  print parserObj.columns
